1ede1f4430e63f679bc5ba4b78b4194c2ba37730
sharonshiju5 <sharonshiju261@gmail.com>
2025-04-12 14:13:50 +0530

    host

--- serverside/server.js
+++ serverside/server.js
@@ -3,6 +3,7 @@ import cors from "cors"
 import connection from "./connection.js";
 import env from "dotenv"
 import router from "./router.js";
+import path from "path";
 
 
 env.config()
@@ -14,6 +15,12 @@ const app=express();
 app.use(cors())
 app.use(express.json({limit:"100mb"}))
 app.use("/api",router)
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 connection().then(()=>{
     app.listen(process.env.PORT,()=>{
         console.log(`server started on http://localhost:${process.env.PORT}`);
